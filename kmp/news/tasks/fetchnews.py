db3d0fbd66bd7bda2bb65b60d3596a6209b9826b
Sarn Wattanasri <mr.paxton@gmail.com>
2017-05-05 01:18:48 -0700

    Added more helpers for fetching news from TechCrunch: WIP

--- kmp/news/tasks/fetchnews.py
+++ kmp/news/tasks/fetchnews.py
@@ -25,8 +25,10 @@ class FetchNewsJob(CronJobBase):
                     datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + "\n")
             if articles and len(articles) >= 1:
                 for m in articles:
-                    df.write(" :".join(["Model saved", m.source, m]).encode("utf-8") + "\n")
-            df.write("----------------------- \n")
+                    output = ": ".join(["Model saved", m.source, m.title])
+                    # ignore characters that cannot be printed
+                    df.write(output.encode("ascii", "ignore").decode("utf-8"))
+            df.write("\n----------------------- \n")
 
 
 # article models by source and cateogry
@@ -77,3 +79,35 @@ def article_to_model(article, source, category):
         print("\t Error creating a model: " + str(e))
     return a
 
+
+# helper to extract images from TechCrunch news
+def techcrunch_images(url):
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, "html.parser")
+    big_images = [ i.get("src") for i in soup.select("div.article-entry img") ]
+    slide_images = [ i.get("data-src") for i in soup.select("div.slideshowify li div.image img") ]
+    return big_images + slide_images
+
+
+# helper to convert link into unslugified text
+def unslugify(url):
+    if not url:
+        return None
+    url = url.rstrip("/")
+    return url.split("/")[-1].replace("-", " ").capitalize()
+
+
+# helper to extract related links from TechCrunch news
+# returns a dictionary of links that has l.text, l["href"], etc
+def techcrunch_related_links(url):
+    html_doc = requests.get(url).text
+    soup = BeautifulSoup(html_doc, "html.parser")
+    links = soup.select("div.article-entry p a")
+    return [ (l.text, l["href"], unslugify(l.get("href")) ) for l in links ]
+
+
+# input argument is the output type of techcrunch_related_links() method
+def mentioned_topics(t_links):
+    topics = [ max( [ t[0], t[2] ], key=lambda x: len(x) ) for t in t_links ]
+    sorted_topics = sorted(topics, key=lambda x: -len(x))
+    return sorted_topics[:5]
